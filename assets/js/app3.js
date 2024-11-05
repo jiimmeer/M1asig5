caf6d105e26cc8398d437274c8fbeb93f6bba2cd
Jimmer <jimmer@mail.com>
2024-11-05 09:46:13 -0400

    Asignacion 5, Portafolio

---
+++ assets/js/app3.js
@@ -0,0 +1,5 @@
+// JavaScript to add interactivity
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portafolio cargado correctamente.");
+    // Puedes añadir interactividad adicional aquí si es necesario
+});
